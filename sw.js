2c8b3596f0b71dd2db8cfcdaa9b896fd082b9447
Octo-Branch <qkike@icloud.com>
2025-03-11 12:54:06 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,22 @@
-const CACHE_NAME = 'latamaleria-v2';
-const DYNAMIC_CACHE = 'runtime-cache';
+const CACHE_NAME = 'latamaleria-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/src/styles/styles.css',
+  '/src/scripts/script.js',
+  '/public/icons/logo.webp'
+];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll([
-                '/',
-                '/index.html',
-                '/src/styles/styles.css'
-            ]))
-    );
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request)
-            .then(response => response || fetch(e.request))
-);
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+  );
+});
