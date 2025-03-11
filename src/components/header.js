636b341af68d83b803f40ce349f762e763bc14ed
Octo-Branch <qkike@icloud.com>
2025-03-11 12:57:51 -0500

    Create header.js

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+export const renderHeader = () => {
+  return `
+    <header>
+      <nav>
+        <a href="/"><img src="/public/icons/logo.webp" alt="Logo" width="120"></a>
+        <ul>
+          <li><a href="/menu.html">Menú</a></li>
+          <li><a href="/order.html">Ordenar</a></li>
+          <li><a href="/about.html">Nosotros</a></li>
+        </ul>
+      </nav>
+    </header>
+  `;
+};
